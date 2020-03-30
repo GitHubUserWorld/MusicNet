9d43117e969a395a2ea85a717eb22e83d3e30cd5
GitHubUserWorld <jona.sierra@gmail.com>
2020-03-30 23:16:09 +0200

    Aplication commit

--- MusicNet/src/main/java/com/example/MusicNet/MusicNetApplication.java
+++ MusicNet/src/main/java/com/example/MusicNet/MusicNetApplication.java
@@ -2,12 +2,20 @@ package com.example.MusicNet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MusicNetApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MusicNetApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
